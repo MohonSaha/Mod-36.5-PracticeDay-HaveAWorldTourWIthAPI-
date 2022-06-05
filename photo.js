3f53b318d6c7d6eb8339c82bf72ff8a09bd1e1c4
MohonSaha <practicewithmohon@gmail.com>
2022-06-05 22:20:24 +0600

    add new code

--- photo.js
+++ photo.js
@@ -8,14 +8,28 @@ const uploadData = () => {
 
 const displayPhotos = (photos) => {
     const photoContainer = document.getElementById("Photo-Galary");
-    for (photo of photos) {
+    for (const photo of photos) {
     //    console.log(photo);
     const div = document.createElement("div");
     div.innerHTML = `
         <h3> ${photo.title} </h3>
-        <img width="150px" src="${photo.url}" alt="">
+        <img onclick="showDetails('${photo.id}')" width="150px" src="${photo.url}" alt="">
         `;
     photoContainer.appendChild(div);
     
     }
 }
+
+const showDetails = (uniqueId) => {
+    console.log(uniqueId);
+    const url = `https://jsonplaceholder.typicode.com/photos${uniqueId}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayDetails(data))
+}
+
+// const displayDetails = details => {
+//     const displayContainer = document.getElementById("Display-Area");
+//     // console.log(displayContainer);
+
+// }
